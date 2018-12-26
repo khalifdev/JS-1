30d5f1dc223a87cef6af3391d6c6165e541ecbed
khalifdev <n.khalif@yandex.ru>
2018-12-26 15:59:09 +0300

    ДЗ 2

---
+++ func_arifm(z_5).js
@@ -0,0 +1,21 @@
+// Скрипт будет подключаться в задании 6
+
+        // Задание 5
+        function sum(x,y){
+            return x + y;
+        }
+        function razn(x,y){
+            return x - y;
+        }
+        function mult(x,y){
+            return x * y;
+        }
+        function del(x,y){
+            return x / y;
+        }
+//        var a = parseInt(prompt("Введите a"));
+//        var b = parseInt(prompt("Введите b"));
+//        alert("Сумма: " + sum(a,b));
+//        alert("Разность: " + razn(a,b));
+//        alert("Произведение: " + mult(a,b));
+//        alert("Частное: " + del(a,b));
